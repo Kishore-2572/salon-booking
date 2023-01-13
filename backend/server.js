fe45b53d7b3349950a9135b5294bbd06d075ce2e
Kishore-2572 <kishoren2572@gmail.com>
2023-01-13 12:37:14 +0530

    path config in backend

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ app.use(
 app.use(express.json());
 app.use(core());
 
+app.use(express.static(path.join(__dirname,'/frontend/build')));
+app.get("*",(req,res) => {
+  res.sendFile(path.join(__dirname,'/frontend/build/index.html'));
+})
+
 app.use('/api/login', loginRoute);
 app.use('/api/user', userRouter);
 app.use('/api/salon', salonRouter);
